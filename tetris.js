8dff72b488834907957417783e38dfa0c82ef715
EuphoriaBot <dimasfirja80@gmail.com>
2026-07-07 15:55:19 +0800

    Constants

--- tetris.js
+++ tetris.js
@@ -12,6 +12,12 @@ class Tetris {
     moveRight() { }
     moveLeft() { }
     moveBottom() { }
-
     changeRotation() { }
 }
+
+const imageSquareSize = 24;
+const size = 40;
+const framePerSecond = 24;
+const gameSpeed = 5;
+const canvas = document.getElementById("canvas");
+const image = document.getElementById("image");
