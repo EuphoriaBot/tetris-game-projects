c38fa4a03059225f029b627b917a912327205720
EuphoriaBot <dimasfirja80@gmail.com>
2026-07-09 16:06:07 +0800

    Check Right

--- tetris.js
+++ tetris.js
@@ -42,9 +42,19 @@ class Tetris {
     }
 
     checkRight() {
-        if (this.checkRight()) {
-            this.x += 1;
+        for (let i = 0; i < this.template.length; i++) {
+            for (let j = 0; j < this.template.length; j++) {
+                if (this.template[i][j] == 0) continue;
+                let realX = i + this.getTruncedPosition().x;
+                let realY = j + this.getTruncedPosition().y;
+                if (realX - 1 < 0) {
+                    return false;
+                }
+
+                if (gameMap[realY][realX - 1].imageX != -1) return false;
+            }
         }
+        return true;
     }
 
     moveRight() {
