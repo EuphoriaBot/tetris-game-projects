76c62844681dc27ba56cb840ecdd9b784326695b
EuphoriaBot <dimasfirja80@gmail.com>
2026-07-09 16:03:04 +0800

    Check Left

--- tetris.js
+++ tetris.js
@@ -26,6 +26,18 @@ class Tetris {
     }
 
     checkLeft() {
+        for (let i = 0; i < this.template.length; i++) {
+            for (let j = 0; j < this.template.length; j++) {
+                if (this.template[i][j] == 0) continue;
+                let realX = i + this.getTruncedPosition().x;
+                let realY = j + this.getTruncedPosition().y;
+                if (realX - 1 < 0) {
+                    return false;
+                }
+
+                if (gameMap[realY][realX - 1].imageX != -1) return false;
+            }
+        }
         return true;
     }
 
